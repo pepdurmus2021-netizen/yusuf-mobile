1d124ff6f98a517ead9bb54383a5444b8e9ebbd6
pepdurmus2021-netizen <pepdurmus2021@gmail.com>
2026-08-25 13:27:39 +0300

    auto: profile.tsx guncellendi

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -432,7 +432,7 @@ export default function ProfileScreen() {
             <Ionicons name="chevron-forward" size={15} color="#ef4444" />
           </TouchableOpacity>
 
-          <Text style={s.version}>Yusuf Telecom v1.0.0</Text>
+          <Text style={s.version}>BWP v1.0.0</Text>
         </View>
         <View style={{ height: 100 }} />
       </ScrollView>
