f87b27fe7c973f2aab5d753e19bd9eae13479a6f
pepdurmus2021-netizen <pepdurmus2021@gmail.com>
2026-09-03 17:53:56 +0300

    auto: types.js guncellendi

---
+++ brands/types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} BrandConfig
+ * @property {string} key - dosya adıyla aynı, assets/brands/<key>/ ile eşleşmeli
+ * @property {string} appName - telefonda görünen uygulama adı
+ * @property {string} slug - Expo slug (benzersiz olmalı)
+ * @property {string} androidPackage - com.bayiwebpanel.<Marka> şeması
+ * @property {string} adaptiveIconBg - Android adaptive icon arka plan rengi
+ * @property {string} splashBg - splash ekranı arka plan rengi (açık mod)
+ * @property {string} displayName
+ * @property {string} shortInitials
+ * @property {string} logoText
+ * @property {string} legalCompanyName
+ * @property {string} supportWhatsapp
+ * @property {string} supportEmail
+ * @property {string} organizationId
+ */
+module.exports = {};
